13b3643066a7d3c00f9ff203b84fa56cc8479f52
landolabrum <lando@deepturn.com>
2024-02-27 08:15:24 +0000

    Deploy to gh-pages

---
+++ out/_next/static/chunks/pages/profile-beab2d62abfebc4b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[277],{57634:function(n,u,_){"use strict";_.r(u);var t=_(71989);u.default=t.Z},99813:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/profile",function(){return _(57634)}])}},function(n){n.O(0,[5443,7847,9101,7895,2441,1989,2888,9774,179],function(){return n(n.s=99813)}),_N_E=n.O()}]);
---
+++ out/_next/static/chunks/pages/profile/[loc]-655f3fd79ef39417.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5949],{14443:function(n,u,_){"use strict";_.r(u);var t=_(71989);u.default=t.Z},68578:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/profile/[loc]",function(){return _(14443)}])}},function(n){n.O(0,[5443,7847,9101,7895,2441,1989,2888,9774,179],function(){return n(n.s=68578)}),_N_E=n.O()}]);
